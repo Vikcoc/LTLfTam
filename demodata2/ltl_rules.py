a501b40819724f8c7444a759a47537f7174de46b
Vikcoc <vikcoc@yahoo.com>
2026-09-10 17:10:51 +0000

    new demo

--- demodata2/ltl_rules.py
+++ demodata2/ltl_rules.py
@@ -3,13 +3,13 @@ from typing import List
 
 def ltl_rules() -> List[LTLModel]: 
     template = LTLTemplate('precedence')
-    activities_a = ["manualinspection"]
-    activities_b = ["manualissuance"]
+    activities_a = ["Inspect"]
+    activities_b = ["Approve"]
     model = template.fill_template(activities_a, activities_b)
 
-    template2 = LTLTemplate('precedence')
-    activities_a2 = ["automaticinspection"]
-    activities_b2 = ["automaticissuance"]
+    template2 = LTLTemplate('response')
+    activities_a2 = ["Rework"]
+    activities_b2 = ["Inspect"]
     model2 = template2.fill_template(activities_a2, activities_b2)
 
     return [model, model2]
